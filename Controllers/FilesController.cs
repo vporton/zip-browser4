6f0cebde2592b475413a8b61bbb035f36966bd5f
Victor Porton <porton@narod.ru>
2020-02-29 15:46:49 +0200

    probably it works

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using zip_browser4.Models;
+
+namespace zip_browser4.Controllers
+{
+    public class FilesController : Controller
+    {
+        public async Task<IActionResult> Get(String hash, String path)
+        {
+            using (var zipStream = new System.IO.Compression.HttpZipStream("http://remoteArchive.zip"))
+            {
+                string content = "";
+                var entryList = await zipStream.GetEntriesAsync();
+                foreach (var e in entryList) {
+                    await zipStream.ExtractAsync(e, async (entryStream) =>
+                    {
+                        if (e.FileName != path) return;
+                        byte[] buffer = new byte[4096];
+                        while(true) {
+                            int result = await entryStream.ReadAsync(buffer, content.Length, 4096);
+                            if(result == 0) break;
+                            content += buffer.ToString();
+                        }
+                    });
+                }
+                return View(content);
+            }
+        }
+    }
+}
