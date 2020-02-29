d9aa5f421584afec674c7ce669eb85215991854d
Victor Porton <porton@narod.ru>
2020-02-29 15:24:08 +0200

    initial version

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace zip_browser4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
